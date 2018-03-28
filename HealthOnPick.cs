27340612878e057c4cfb9739f1b0b61cd157da7d
Batama <thebatama@gmail.com>
2018-03-28 18:54:13 +0800

    Add files via upload

---
+++ HealthOnPick.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthOnPick : MonoBehaviour {
+	PlayerControl pc;
+	int hp;
+	// Use this for initialization
+	void Start () {
+		hp = 10;
+		Physics.IgnoreLayerCollision (9, 10);//later note: drops and enemies layers
+		//(LayerMask.NameToLayer ("Enemies"), LayerMask.NameToLayer ("Drops"));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0f, 0f, 2f));
+
+	}
+
+	public HealthOnPick(int health)//, Transform loc
+	{
+		hp = health;
+	//	transform.position = loc.position;
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.transform.CompareTag ("Player")) {
+			pc = collision.gameObject.GetComponent<PlayerControl> ();
+			pc.Heal (true, hp);
+			//play particle effect
+			Debug.Log (collision.collider.tag);
+			gameObject.SetActive (false);//(gameObject);
+		} 
+		//else if (collision.transform.CompareTag ("Enemy")) {
+		//	gameObject.GetComponent<Rigidbody> ().isKinematic = true;
+		////	collision.rigidbody.isKinematic = true;
+		//}
+		return;
+	}
+
+	//void OnCollisionExit(Collision collision)
+	//{
+	////	if (collision.transform.CompareTag ("Enemy"))  collision.rigidbody.isKinematic = false;
+	//	gameObject.GetComponent<Rigidbody> ().isKinematic = false;
+	//}
+}
---
+++ PlayerShootRay.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+public class PlayerShootRay : MonoBehaviour
+{
+	public int damagePerShot = 20;
+	public float timeBetweenBullets = 0.15f;
+	public float range = 10f;
+	public Transform gunPoint;
+	Transform gunHand;
+
+	float timer;
+	Ray shootRay;
+	RaycastHit shootHit;
+//	int shootableMask;
+	//	ParticleSystem gunParticles;
+	LineRenderer gunLine;
+	//	AudioSource gunAudio;
+	Light gunLight;
+	float effectsDisplayTime = 0.2f;
+		
+	void Awake ()
+	{
+		gunHand = GameObject.FindGameObjectWithTag ("GunHand").GetComponent<Transform> ();
+//		shootableMask = LayerMask.GetMask ("Floor");//("Shootable");
+		//	gunParticles = GetComponent<ParticleSystem> ();
+		gunLine = GetComponent <LineRenderer> ();
+		//		gunAudio = GetComponent<AudioSource> ();
+		gunLight = GetComponent<Light> ();
+	}
+
+//	void OnEnable()
+//	{
+//		transform.position = gunHand.position;
+//		MeshRenderer mr = GetComponent<MeshRenderer> ();
+//		Debug.Log (mr.enabled);
+//	}
+
+	void Update ()
+	{
+		transform.position = gunHand.position;
+		transform.rotation = gunHand.rotation;
+		timer += Time.deltaTime;
+
+		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+		{
+			Shoot();
+		}
+
+		if(timer >= timeBetweenBullets * effectsDisplayTime)
+		{
+			DisableEffects ();
+		}
+	}
+
+
+	public void DisableEffects ()
+	{
+		gunLine.enabled = false;
+		gunLight.enabled = false;
+	}
+
+
+	void Shoot()
+	{
+		timer = 0f;
+
+		//		gunAudio.Play ();
+	//	gunLight.transform.position = gunPoint.position;
+		gunLight.enabled = true;
+
+		//	gunParticles.Stop ();
+		//	gunParticles.Play ();
+
+		gunLine.enabled = true;
+		//	gunLine.SetPosition (0, transform.position);
+		//	shootRay.origin = transform.position;
+		//	shootRay.direction = transform.forward;
+		gunLine.SetPosition (0, gunPoint.position);
+		shootRay.origin = gunPoint.position;
+		shootRay.direction = gunPoint.forward;
+		//	
+		if(Physics.Raycast (shootRay, out shootHit, range))
+		{
+			EnemyMovement enemyHealth = shootHit.collider.GetComponent <EnemyMovement> ();
+			//if(enemyHealth != null)
+				//	if(shootHit.collider.CompareTag("Enemy"))
+			//if (losHit.transform.CompareTag ("Player"))
+			if(shootHit.transform.CompareTag("Enemy") && enemyHealth != null)
+			{
+				Debug.Log ("hit an enemy");
+				enemyHealth.TakeDamage (damagePerShot, shootHit.point);			
+			}
+			gunLine.SetPosition (1, shootHit.point);
+		}
+		else
+		{
+			gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+		}
+	}
+}
