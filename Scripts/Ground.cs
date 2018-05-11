905d2654b701b4c4313011cfb61c1328b6a0341b
Batama <thebatama@gmail.com>
2018-05-11 15:59:05 +0800

    Update 5_11
    Added Models: trees, rocks
    Added Camera Control

---
+++ Scripts/Ground.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ground : MonoBehaviour {
+	public	List<GameObject> dropList = new List<GameObject>();
+	public int listSize = 2;
+	public int spareSlot;
+	GameObject obj;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < listSize; i++) {
+			GameObject o1 = new GameObject ();
+			o1.SetActive (false);
+			dropList.Add (o1);
+		}
+		spareSlot = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	//TODO: dropChance - random drop chance
+	public void addToList(Transform tform, int type, float dropChance)//GameObject obj, 
+	{
+		float r = Random.Range(0.0f, 1.0f);
+	//	obj = Instantiate (Resources.Load ("HealthOnPick"), tform.position, tform.rotation) as GameObject;
+	//	obj.transform.position = tform.position;
+	//	obj.SetActive (false);
+		if (r < dropChance) {
+			dropList [spareSlot].SetActive (false);
+			Destroy (dropList [spareSlot]);
+			dropList [spareSlot] = Instantiate (Resources.Load ("HealthOnPick"), tform.position, tform.rotation) as GameObject;
+			dropList [spareSlot].transform.position = tform.position;
+		//	if (dropList [spareSlot].activeInHierarchy) {
+			dropList [spareSlot].SetActive (true);
+			spareSlot++;
+			if (spareSlot == listSize) {
+				spareSlot = 0;
+			}
+		//	}
+		//	dropList [spareSlot].SetActive (true);
+		}
+		Debug.Log ("Dropped!");
+	}
+}
---
+++ Scripts/Item.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item{
+
+	public string itemName;
+	public int itemID;
+	public string itemDesc;
+	public Texture2D itemIcon;
+	public int itemPower;
+	public int itemSpeed;
+	public ItemType itemType;
+	public DropType dropType;
+
+	public enum ItemType
+	{
+		Weapon,
+		Consumable,
+		Quest,
+		Outfit
+	}
+
+	public enum DropType
+	{
+		ConsumeOnPick,
+		ConsumeOnUse,
+		None
+	}
+
+	public Item(string name, int id, string desc, int power, int speed, ItemType type, DropType dtype)
+	{
+		itemName = name;
+		itemID = id;
+		itemDesc = desc;
+		itemIcon = Resources.Load<Texture2D>("Textures/"+name);
+		itemPower = power;
+		itemSpeed = speed;
+		itemType = type;
+		dropType = dtype;
+	}
+	public Item()
+	{
+		itemName = null;
+		itemID = -1;
+	}
+}
